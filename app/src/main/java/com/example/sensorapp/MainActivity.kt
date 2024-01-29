135058abb8e20ebdd649fecff1434eee41f5f4d3
yuki <ywatanabe6229@gmail.com>
2024-01-29 12:40:16 +0900

    Added send button

--- app/src/main/java/com/example/sensorapp/MainActivity.kt
+++ app/src/main/java/com/example/sensorapp/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.sensorapp
 // MainActivity.kt
 import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+import androidx.core.content.FileProvider
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         val startRecordingButton: Button = findViewById(R.id.startRecordingButton)
         val stopRecordingButton: Button = findViewById(R.id.stopRecordingButton)
+        val sendDataButton: Button = findViewById(R.id.sendDataButton)
         val nameEditText: EditText = findViewById(R.id.nameEditText)
 
         val sensorService = Intent(this, SensorService::class.java)
@@ -31,11 +35,43 @@ class MainActivity : AppCompatActivity() {
             startForegroundService(sensorService)
         }
 
-
         stopRecordingButton.setOnClickListener {
             //書き込むのをやめる
             Log.d("com.example.sensorapp.MainActivity", "Stop Button clicked!")
             stopService(sensorService)
+        }
+
+        sendDataButton.setOnClickListener {
+            Log.d(
+                "com.example.sensorapp.MainActivity",
+                "${applicationContext.packageName}.fileprovider"
+            )
+
+            // センサデータを記録したファイル一覧を取得
+            val filesDir = applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+            val files = filesDir?.listFiles()?.map { file ->
+                FileProvider.getUriForFile(
+                    this@MainActivity, "com.example.sensorapp.fileprovider", file
+                )
+            }
+
+            files?.forEach { fileUri ->
+                grantUriPermission(
+                    packageName,
+                    fileUri,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION,
+                )
+            }
+
+            files?.let {
+                val shareIntent = ShareCompat.IntentBuilder(this).apply {
+                    setChooserTitle("Send CSV files to ...")
+                    files.forEach { file ->
+                        addStream(file)
+                    }
+                    setType("text/csv")
+                }.startChooser()
+            }
         }
     }
 }
